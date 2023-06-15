48808c81af251e8e646c1a77d0886d27eeb903c1
hjongc <chj7785@gmail.com>
2023-06-15 21:24:40 +0900

    Feat: 블로그 회원가입, 로그인, 로그아웃 구현

--- blog-project/README.md
+++
@@ -1,3 +0,0 @@
-# study-springboot-developer
-
-현재 7장까지의 실습코드를 진행했습니다.
---
+++ blog-project/src/main/java/me/hjongc/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,21 @@
+package me.hjongc.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.hjongc.springbootdeveloper.domain.User;
+import me.hjongc.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+// 스프링 시큐리티에서 사용자 정보를 가져오는 인터페이스
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    // 이메일로 사용자 정보 가져오는 메서드
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
