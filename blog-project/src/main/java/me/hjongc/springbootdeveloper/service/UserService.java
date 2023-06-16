822b77360b3821830b3484b553a8fd3fc8bcabdb
hjongc <chj7785@gmail.com>
2023-06-16 16:38:53 +0900

    Feat: JWT 로그인, 로그아웃 구현

--- blog-project/src/main/java/me/hjongc/springbootdeveloper/service/UserService.java
+++ blog-project/src/main/java/me/hjongc/springbootdeveloper/service/UserService.java
@@ -10,14 +10,25 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class UserService {
+
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public Long save(AddUserRequest dto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
-                .password(bCryptPasswordEncoder.encode(dto.getPassword())) // 패스워드 암호화
-                .build())
-                .getId();
+                .password(encoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+
+    public User findById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
     }
 }
