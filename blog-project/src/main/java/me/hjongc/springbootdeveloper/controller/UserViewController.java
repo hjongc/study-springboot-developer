f7037fa92cb5ba19a8ed805299b066d59f0dc403
hjongc <chj7785@gmail.com>
2023-06-22 23:49:49 +0900

    Feat: AWS 배포 완료

--- blog-project/src/main/java/me/hjongc/springbootdeveloper/controller/UserViewController.java
+++ blog-project/src/main/java/me/hjongc/springbootdeveloper/controller/UserViewController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserViewController {
     @GetMapping("/login")
     public String login() {
-        return "oauthlogin";
+        return "oauthLogin";
+        // return "oauthlogin";
+        // -> local에서는 대소문자 구분없이 잘 작동하였음.
+        // -> 하지만, AWS 배포 환경에서는 대소문자 구분해줘야함!!!
     }
 
     @GetMapping("/signup")
